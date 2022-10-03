4e5d816599fe34afb2165e7130c4aee215b99312
Aditya Arya <52771727+Aditya9111@users.noreply.github.com>
2022-10-03 21:48:19 +0530

    changed app.py

--- app.py
+++ app.py
@@ -223,5 +223,5 @@ def outputMap():
 
 # start the server with the 'run()' method
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(debug=True)
     # app.run(debug=True) #will run locally http://127.0.0.1:5000/
